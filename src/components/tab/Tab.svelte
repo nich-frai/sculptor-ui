2791552d8d1aeb53c571abd48dcaf3dd6b84e884
Nicholas Frai <107412954+nich-frai@users.noreply.github.com>
2022-08-22 20:05:45 -0300

    desired components files created

---
+++ src/components/tab/Tab.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+
+  import { TabController } from "./tab.controller";
+
+  const controller = new TabController();
+  setContext("tab", controller);
+</script>
+
+<div class="tab">
+  <slot />
+</div>
---
+++ src/components/tab/TabViewport.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { setContext } from "svelte";
+
+  import { TabViewportController } from "./tab_viewport.controller";
+
+  const controller = new TabViewportController();
+
+  setContext("tab-viewport", controller);
+</script>
+
+<div class="tabbed-viewport">
+  <slot />
+</div>
